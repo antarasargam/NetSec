861ccb2accdb56cb7a12802556cc75730cfe7fff
antarasargam <antarasargam@gmail.com>
2017-09-15 11:55:27 -0400

    Update Client.py

--- lab_1d/Client.py
+++ lab_1d/Client.py
@@ -1,4 +1,5 @@
 import asyncio
+import sys
 import playground
 from playground.network.packet import PacketType
 from playground.network.packet.fieldtypes import UINT32, BUFFER, STRING
@@ -9,6 +10,7 @@ import random
 
 #from playground.common import logging as p_logging
 #p_logging.EnablePresetLogging(p_logging.PRESET_TEST)
+
 
 # Client Requesting for Conversion
 
@@ -41,23 +43,29 @@ class Conversion(PacketType):
 
 class MyClient(asyncio.Protocol):
 
-    def __init__(self, loop, message):
-        self.loop = loop
-        self._transport = None
-        self.message = message
+    def __init__(self, callback = None):
+        self.buffer = ""
+        if callback:
+            self.callback = callback
+            print("It's in callback_1")
+        else:
+            self.callback = print
+
+        self.transport = None
+        self.deserializer = PacketType.Deserializer()
+
+    def close(self):
+        self.__sendMessageActual("__QUIT__")
 
     def connection_made(self, transport):
-        self._transport = transport
         print("Client Connection made!")
-        self.message = message.__serialize__()
-        print (self.message)
-        self._transport.write(self.message)
-        self._deserializer = PacketType.Deserializer()
+        self.transport = transport
+        self.deserializer = RequestConversion.Deserializer()
 
     def data_received(self, data):
-        self._deserializer = PacketType.Deserializer()
-        self._deserializer.update(data)
-        for packet in self._deserializer.nextPackets():
+        self.deserializer = PacketType.Deserializer()
+        self.deserializer.update(data)
+        for packet in self.deserializer.nextPackets():
             print (packet)
             if (packet.DEFINITION_IDENTIFIER == "Antara.Packet2"):
 
@@ -68,29 +76,54 @@ class MyClient(asyncio.Protocol):
                 ClientPacket2.value = self.input_value()
                 ClientPacket2bytes = ClientPacket2.__serialize__()
                 #pair[packet.SessionID] = "Data_Sent"
-                self._transport.write(ClientPacket2bytes)
-                print("Found packet - 1 from server!")
+                self.transport.write(ClientPacket2bytes)
 
             elif (isinstance(packet, Conversion)):
                 print ("The final value received is: ",packet.finalvalue)
 
             else:
                 print ("No packet")
-                self._transport.close()
+                self.transport.close()
 
     def input_value(self):
         self.input_metric = input ("What value to convert: ")
         return self.input_metric
 
+    def send(self):
+
+        packet = RequestConversion()
+        self.transport.write(packet.__serialize__())
+
     def connection_lost(self, exc):
-        self._transport = None
+        self.transport = None
 
-loop = asyncio.get_event_loop()
+class ControlProtocol:
+    def __init__(self):
+        self.txProtocol = None
 
-message = RequestConversion()
-playground.getConnector().create_playground_connection(lambda: MyClient(loop,message), '20174.1.1.2', 101)
+    def buildProtocol(self):
+        return MyClient(self.callback)
 
-#loop.run_until_complete()
-loop.run_forever()
+    def connect(self, txProtocol):
+        print ("Calling connect")
+        self.txProtocol = txProtocol
+        print ("Connection to Server established")
+        self.txProtocol = txProtocol
+        self.txProtocol.send()
+        #asyncio.get_event_loop().add_reader(self.param, self.stdinAlert)
 
-loop.close()
+    def callback(self):
+        print ("this is the message")
+        sys.stdout.flush()
+
+
+if __name__ == "__main__":
+
+    loop = asyncio.get_event_loop()
+    loop.set_debug(enabled=True)
+    control = ControlProtocol()
+    coro = playground.getConnector().create_playground_connection(control.buildProtocol, '20174.1.1.1', 101)
+    transport, protocol = loop.run_until_complete(coro)
+    control.connect(protocol)
+    loop.run_forever()
+    loop.close()
