b71b400b42c0d79eda03a104759e1cf01f5b0262
antarasargam <antarasargam@gmail.com>
2017-10-04 21:05:00 -0400

    Update __init__.py

--- lab2/src/lab2_protocol/__init__.py
+++ lab2/src/lab2_protocol/__init__.py
@@ -1,5 +1,6 @@
 import playground
-from .
+from .Server import Serverfactory
+from .Client import Clientfactory
 
 lab2Connector = playground.Connector(protocolStack=(Clientfactory, Serverfactory))
 playground.setConnector("lab2_protocol", lab2Connector)
